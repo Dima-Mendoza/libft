cb9443c6638ade7704b20978b63901b94cd41b6d
Five <spartanskijmapper534@gmail.com>
2025-11-27 16:49:08 +0300

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -4,21 +4,14 @@
 
 #include "libft.h"
 
-void *ft_memset(void *s, int c, size_t n) {
-// переменная которой все заполняем
-// приводим тип s к байтам
-// с помощью цикла переписываем область памяти до n
-// возврашаем эту область памяти
+void *ft_memset(void *s, int value, size_t n) {
+    char *val = (char *) s;
+    size_t i = 0;
 
-unsigned char *p;
-p = (unsigned char *)s;
+    while (i < n) {
+        val[i] = (char)value;
+        i++;
+    }
 
-size_t i = 0;
-
-while (i < n) {
-    p[i] = (unsigned char)c;
-    i++;
+    return s;
 }
-
-return (s);
-};
