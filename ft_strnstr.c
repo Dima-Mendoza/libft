e08f2fffa76d57a8922db81409df85c011a0cbac
Dima-Mendoza <spartanskijmapper534@gmail.com>
2026-01-18 22:22:56 +0300

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -7,17 +7,15 @@ char *ft_strnstr(const char *big, const char *little, size_t len) {
 
     size_t i = 0;
     size_t j = 0;
-    size_t start = 0;
-    size_t len_little = ft_strlen(little);
 
-    while (big[i] != '\0') {
-        start = i;
-        while (big[i] == little[j]) {
-            if (little[j] == '\0' && j == len_little) {
-                char *result = (char *)big[start];
+    if (little[0] == '\0') return big;
+
+    while (big[i] != '\0' && i < len) {
+        while (big[i+j] == little[j] && i + j < len) {
+            if (little[j+1] == '\0') {
+                char *result = (char *)(big + i);
                 return result;
             };
-            i++;
             j++;
         }
         j = 0;
