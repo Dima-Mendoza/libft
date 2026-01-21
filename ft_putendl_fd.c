65a415ab440b8d8319a4a95cfd07892586daafab
Five <spartanskijmapper534@gmail.com>
2026-01-21 22:38:38 +0300

    putnbr && putnbr_fd

---
+++ ft_putendl_fd.c
@@ -0,0 +1,10 @@
+//
+// Created by Mendoza on 1/21/2026.
+//
+#include "libft.h"
+
+void ft_putendl_fd(char const *s, int fd) {
+    if(!s) return;
+    write(fd, s, ft_strlen(s));
+    write(fd, "\n", 1);
+}
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,21 +3,21 @@
 //
 #include "libft.h"
 
-void ft_putnbr(int n) {
+void ft_putnbr_fd(int n, int fd) {
     if (n == -2147483648) {
-        write(1, "-2147483648", 11);
+        write(fd, "-2147483648", 11);
         return;
     }
     if (n < 0) {
         n = n * -1;
-        write(1, "-", 1);
+        write(fd, "-", 1);
     }
     char c;
 
     if (n >= 10) {
-        ft_putnbr(n / 10);
+        ft_putnbr_fd(n / 10, fd);
     }
 
     c = (n % 10) + '0';
-    write(1, &c, 1);
+    write(fd, &c, 1);
 }
